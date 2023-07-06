d59e926de11a8c398061ab78f6d6592a95d21ed3
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-07-06 19:57:34 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export interface IW3bstreamClient {
-  publish: (deviceId: string, data: Object, eventType?: string) => Promise<any>;
+  publish: (deviceId: string, data: Object, eventType?: string) => Promise<Response>;
 }
 
 interface SingleMsg {
