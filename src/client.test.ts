ac6f2f71ef90e88d073c392dbeed05f7c4a12854
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-07-13 20:54:24 -0300

    update client
    update request body
    add batch publishing

--- src/client.test.ts
+++ src/client.test.ts
@@ -1,40 +1,19 @@
 import axios from "axios";
 
-import { W3bstreamClient, IW3bstreamClient } from "./";
+import { W3bstreamClient, IW3bstreamClient, WSHeader, WSPayload } from "./";
 
 const MOCK_URL = "http://localhost:8080";
 const MOCK_API_KEY = "1234567890";
 const MOCK_DEVICE_ID = "1234567890";
+const MOCK_DEVICE_ID_2 = "0987654321";
 const MOCK_EVENT_TYPE = "DEFAULT";
 const MOCK_DATA = { test: "test" };
-
-const mockFetch = jest.spyOn(axios, "post").mockImplementation(() => {
-  return Promise.resolve([
-    {
-      index: 0,
-      results: [
-        {
-          appletName: "1000",
-          instanceID: "1000",
-          handler: "start",
-          returnValue: null,
-          code: 0,
-        },
-      ],
-    },
-  ]);
-});
-
-const initClient = (): IW3bstreamClient =>
-  new W3bstreamClient(MOCK_URL, MOCK_API_KEY);
+const _DATA_PUSH_EVENT_TYPE = "DA-TA_PU-SH";
 
 describe("W3bstreamClient", () => {
-  describe("Contructor", () => {
+  describe("constructor", () => {
     it("should initialize", () => {
-      const client: IW3bstreamClient = new W3bstreamClient(
-        MOCK_URL,
-        MOCK_API_KEY
-      );
+      const client = new W3bstreamClient(MOCK_URL, MOCK_API_KEY);
       expect(client).toBeDefined();
     });
     it("should throw if no url", () => {
@@ -48,54 +27,121 @@ describe("W3bstreamClient", () => {
       );
     });
   });
-  describe("Publishing", () => {
-    it("should publish single msg", () => {
-      const client = initClient();
+  describe("publishing", () => {
+    let client: IW3bstreamClient;
+    let mockFetch: jest.SpyInstance;
+    beforeEach(() => {
+      mockFetch = jest.spyOn(axios, "post").mockImplementation(() => {
+        return Promise.resolve([
+          {
+            index: 0,
+            results: [
+              {
+                appletName: "1000",
+                instanceID: "1000",
+                handler: "start",
+                returnValue: null,
+                code: 0,
+              },
+            ],
+          },
+        ]);
+      });
 
-      const header = {
-        deviceId: MOCK_DEVICE_ID,
-        eventType: MOCK_EVENT_TYPE,
+      client = new W3bstreamClient(MOCK_URL, MOCK_API_KEY);
+    });
+    afterEach(() => {
+      mockFetch.mockRestore();
+    });
+    it("should publish single msg", async () => {
+      const header: WSHeader = {
+        device_id: MOCK_DEVICE_ID,
+        event_type: MOCK_EVENT_TYPE,
+        timestamp: Date.now(),
       };
 
       client.publish(header, MOCK_DATA);
-
-      expect(mockFetch).toHaveBeenCalled();
+      expect(mockFetch).toHaveBeenCalledWith(
+        `${MOCK_URL}?eventType=${_DATA_PUSH_EVENT_TYPE}&timestamp=${header.timestamp}`,
+        [
+          {
+            device_id: header.device_id,
+            event_type: header.event_type,
+            payload: JSON.stringify(MOCK_DATA),
+            timestamp: header.timestamp,
+          },
+        ],
+        {
+          headers: {
+            Authorization: `Bearer ${MOCK_API_KEY}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
     });
-    it("should publish single msg without event type", () => {
-      const client = initClient();
-
-      const header = {
-        deviceId: MOCK_DEVICE_ID,
+    it("should publish single msg without event type", async () => {
+      const header: WSHeader = {
+        device_id: MOCK_DEVICE_ID,
       };
 
       client.publish(header, MOCK_DATA);
-
-      expect(mockFetch).toHaveBeenCalled();
+      expect(mockFetch).toHaveBeenCalledTimes(1);
     });
-    it("should throw if no device id", () => {
-      const client = initClient();
-
-      const header = {
-        deviceId: "",
-        eventType: MOCK_EVENT_TYPE,
+    it("should throw if no device id", async () => {
+      const header: WSHeader = {
+        device_id: "",
+        event_type: MOCK_EVENT_TYPE,
       };
 
-      expect(() => client.publish(header, MOCK_DATA)).rejects.toThrow(
+      await expect(client.publish(header, MOCK_DATA)).rejects.toThrow(
         "W3bstreamClient: device id is required"
       );
     });
     it("should publish single msg with binary data", () => {
-      const client = initClient();
-
-      const header = {
-        deviceId: MOCK_DEVICE_ID,
-        eventType: MOCK_EVENT_TYPE,
+      const header: WSHeader = {
+        device_id: MOCK_DEVICE_ID,
+        event_type: MOCK_EVENT_TYPE,
       };
 
       const data = Buffer.from("test data", "utf8");
       client.publish(header, data);
 
-      expect(mockFetch).toHaveBeenCalled();
+      expect(mockFetch).toHaveBeenCalledTimes(1);
+    });
+    it("should publish message batch", () => {
+      const msgs: WSPayload = [
+        {
+          device_id: MOCK_DEVICE_ID,
+          event_type: MOCK_EVENT_TYPE,
+          payload: JSON.stringify(MOCK_DATA),
+          timestamp: Date.now(),
+        },
+        {
+          device_id: MOCK_DEVICE_ID,
+          event_type: MOCK_EVENT_TYPE,
+          payload: JSON.stringify(MOCK_DATA),
+          timestamp: Date.now(),
+        },
+        {
+          device_id: MOCK_DEVICE_ID_2,
+          event_type: MOCK_EVENT_TYPE,
+          payload: JSON.stringify(MOCK_DATA),
+          timestamp: Date.now(),
+        },
+      ];
+      const timestamp = Date.now();
+      client.publishBatch(msgs, timestamp);
+
+      expect(mockFetch).toHaveBeenCalledWith(
+        `${MOCK_URL}?eventType=${_DATA_PUSH_EVENT_TYPE}&timestamp=${timestamp}`,
+        msgs,
+        {
+          headers: {
+            Authorization: `Bearer ${MOCK_API_KEY}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
     });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
 import axios, { AxiosResponse } from "axios";
 
 export interface WSHeader {
-  deviceId: string;
-  eventType?: string;
+  device_id: string;
+  event_type?: string;
   timestamp?: number;
 }
+
+interface WSMessage {
+  device_id: string;
+  event_type: string;
+  payload: string;
+  timestamp: number;
+}
+
+export interface WSPayload extends Array<WSMessage> {}
 
 export interface IW3bstreamClient {
   publish: (
     header: WSHeader,
     payload: Object | Buffer
   ) => Promise<AxiosResponse>;
+  publishBatch: (
+    payload: WSPayload,
+    timestamp?: number
+  ) => Promise<AxiosResponse>;
 }
 
 export class W3bstreamClient implements IW3bstreamClient {
+  private _DATA_PUSH_EVENT_TYPE = "DA-TA_PU-SH";
   constructor(private _url: string, private _apiKey: string) {
     if (!_url) {
       throw new Error("W3bstreamClient: url is required");
@@ -30,26 +44,50 @@ export class W3bstreamClient implements IW3bstreamClient {
     header: WSHeader,
     payload: Object | Buffer
   ): Promise<AxiosResponse> {
-    if (!header.deviceId) {
+    if (!header.device_id) {
       throw new Error("W3bstreamClient: device id is required");
     }
 
-    const url = this._buildUrl(header);
-    return this._publish(url, payload);
+    const payloadObj = this._buildPayload(header, payload);
+
+    return this._publish(payloadObj, header.timestamp);
   }
 
-  private _buildUrl({
-    deviceId,
-    eventType = "DEFAULT",
-    timestamp = Date.now(),
-  }: WSHeader): string {
-    return `${this._url}?device_id=${deviceId}&eventType=${eventType}&timestamp=${timestamp}`;
+  public async publishBatch(
+    payload: WSPayload,
+    timestamp?: number
+  ): Promise<AxiosResponse> {
+    return this._publish(payload, timestamp);
+  }
+
+  private _buildUrl(timestamp: number = Date.now()): string {
+    return `${this._url}?eventType=${this._DATA_PUSH_EVENT_TYPE}&timestamp=${timestamp}`;
+  }
+
+  private _buildPayload(header: WSHeader, payload: Object | Buffer): WSPayload {
+    const {
+      device_id,
+      event_type = "DEFAULT",
+      timestamp = Date.now(),
+    } = header;
+    const _payload =
+      payload instanceof Buffer ? payload.toString() : JSON.stringify(payload);
+    return [
+      {
+        device_id,
+        event_type,
+        payload: _payload,
+        timestamp,
+      },
+    ];
   }
 
   private _publish(
-    url: string,
-    payload: Object | Buffer
+    payload: WSPayload,
+    timestamp?: number
   ): Promise<AxiosResponse> {
+    const url = this._buildUrl(timestamp);
+
     return axios.post(url, payload, {
       headers: {
         Authorization: `Bearer ${this._apiKey}`,
